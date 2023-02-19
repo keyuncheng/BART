94068511c7a82c2baff6626dbdc9afa2917bf7c5
Keyun Cheng <keyun_cheng@qq.com>
2023-02-19 11:53:59 +0800

    [mod] fix for SM-G - fix send load calculation

--- README.md
+++ README.md
@@ -3,10 +3,6 @@ Balanced Code Conversion
 
 ## Known issues
 
-* When calculating costs, if m > 1, the cost for each parity block are
-      calculated with ascending order of id, instead of ascending order of
-      cost; not sure this can this be fixed?
-
 * When number of stripes goes very large, the combinations cannot be
   represented by int
     * Type: int -> uint64_t
--- src/model/StripeMergeG.cc
+++ src/model/StripeMergeG.cc
@@ -59,93 +59,28 @@ void StripeMergeG::getSolutionForStripeGroup(StripeGroup &stripe_group, vector<v
     //     printf("parity block %d:\n", parity_id);
     //     Utils::printIntVector(parity_distributions[parity_id]);
     // }
-    
-    // TODO: Step 1: data relocation; Step 2: parity relocation
 
-    // relocated stripe distribution
+
+    // relocated block distribution
     vector<bool> relocated_blk_distribution(num_nodes, false);
 
-    // Step 1: data relocation
+    // candidate nodes for block relocation
+    vector<int> reloc_node_candidates;
 
-    // candidate nodes for data relocation (no any data block stored on that node)
-    vector<int> data_reloc_candidates;
     for (int node_id = 0; node_id < num_nodes; node_id++) {
-        // find nodes store more than lambda_f data block (in parity merging, lambda_f = 1, we find nodes where no data block is stored, and no parity block is relocated)
-        if (data_distribution[node_id] < code.lambda_f) {
-            data_reloc_candidates.push_back(node_id);
-        } else {
-            // mark the block as relocated
+        // mark nodes with at least one data block as relocated with a data block
+        if (data_distribution[node_id] > 0) {
             relocated_blk_distribution[node_id] = true;
+        } else {
+            reloc_node_candidates.push_back(node_id);
         }
     }
 
-    // printf("data relocation candidates:\n");
-    // Utils::printIntVector(data_reloc_candidates);
-
-    // record data blocks that needs relocation (stripe_id, block_id)
-    vector<pair<int, int> > data_blocks_to_reloc;
-
-    // relocate data blocks
-    for (int node_id = 0; node_id < num_nodes; node_id++) {
-        // find nodes store more than 1 data block
-        if (data_distribution[node_id] <= code.lambda_f) {
-            continue;
-        }
-        int num_db_overlapped = 0;
-        for (int stripe_id = 0; stripe_id < code.lambda_i; stripe_id++) {
-            vector<int> &stripe_indices = stripes[stripe_id]->getStripeIndices();
-            for (int block_id = 0; block_id < code.k_i; block_id++) {
-                // find block_id that stored at node_id
-                if (stripe_indices[block_id] != node_id) {
-                    continue;
-                }
-                num_db_overlapped++;
-                // do relocation starting from the 2nd overlapped data block
-                if (num_db_overlapped > code.lambda_f) {
-                    data_blocks_to_reloc.push_back(pair<int, int>(stripe_id, block_id));
-                }
-            }
-        }
-    }
-
-    // create data relocation task for each data block
-    for (auto &item : data_blocks_to_reloc) {
-        int stripe_id = item.first;
-        int block_id = item.second;
-
-        vector<int> &stripe_indices = stripes[stripe_id]->getStripeIndices();
-        int node_id = stripe_indices[block_id];
-
-        // Strategy 1: randomly pick a node from data relocation candidates
-        int pos = Utils::randomInt(0, data_reloc_candidates.size() - 1, random_generator);
-
-        // // Strategy 2: pick the first node from data relocation candidates
-        // int pos = 0;
-
-        // relocate node id
-        int reloc_node_id = data_reloc_candidates[pos];
-
-        // mark the block as relocated
-        relocated_blk_distribution[reloc_node_id] = true;
-
-        // remove the element from data relocation candidate
-        data_reloc_candidates.erase(data_reloc_candidates.begin() + pos);
-        // update the data distribution
-        data_distribution[node_id]--;
-        data_distribution[reloc_node_id]++;
-
-        // create a data relocation task
-        vector<int> solution;
-        solution.push_back(stripe_id);
-        solution.push_back(block_id);
-        solution.push_back(node_id);
-        solution.push_back(reloc_node_id);
-
-        solutions.push_back(solution);
-    }
+    // printf("node candidates for block relocation (before PM):\n");
+    // Utils::printIntVector(reloc_node_candidates);
 
 
-    // Step 2: parity relocation
+    // Step 1: parity relocation
 
     // all nodes are candidates for parity block computation
     vector<int> parity_comp_candidates(num_nodes, 0);
@@ -204,23 +139,28 @@ void StripeMergeG::getSolutionForStripeGroup(StripeGroup &stripe_group, vector<v
 
         // check if parity_comp_node_id already stores a block
         if (relocated_blk_distribution[parity_comp_node_id] == false) {
-            // if the node doesn't store a block, then we don't need to relocate
-            // mark as relocated
+            // if the node doesn't store a block, then we don't need to relocate to other nodes
+
+            // mark the node as relocated
             relocated_blk_distribution[parity_comp_node_id] = true;
+            
+            // remove from candidate node list
+            auto it = find(reloc_node_candidates.begin(), reloc_node_candidates.end(), parity_comp_node_id);
+            if (it != reloc_node_candidates.end()) {
+                reloc_node_candidates.erase(it);
+            }
         } else {
             // if the node stores a parity block, then we need to relocate to another node
 
-            // candidate nodes for parity block relocation (nodes without a block)
-            vector<int> parity_reloc_candidates;
-            for (int node_id = 0; node_id < num_nodes; node_id++) {
-                if (relocated_blk_distribution[node_id] == false) {
-                    parity_reloc_candidates.push_back(node_id);
-                }
-            }
-
             // randomly pick one node with minimum cost to compute
-            int random_pos = Utils::randomInt(0, parity_reloc_candidates.size() - 1, random_generator);
-            int parity_reloc_node_id = parity_reloc_candidates[random_pos];
+            int random_pos = Utils::randomInt(0, reloc_node_candidates.size() - 1, random_generator);
+            int parity_reloc_node_id = reloc_node_candidates[random_pos];
+
+            // mark the node as relocated with a block
+            relocated_blk_distribution[parity_reloc_node_id] = true;
+
+            // remove the node from candidate list
+            reloc_node_candidates.erase(reloc_node_candidates.begin() + random_pos);
 
             // create a parity relocation task
             vector<int> solution;
@@ -234,13 +174,75 @@ void StripeMergeG::getSolutionForStripeGroup(StripeGroup &stripe_group, vector<v
         
     }
 
-    // printf("parity_relocated_nodes:\n");
-    // Utils::printIntVector(parity_relocated_nodes);
+    // printf("node candidates for block relocation (after PM):\n");
+    // Utils::printIntVector(reloc_node_candidates);
 
 
+    // Step 2: data relocation
 
-    // // now the remaining data relocation nodes will be used for parity merging
-    // printf("data relocation candidates (after data relocation):\n");
-    // Utils::printIntVector(data_reloc_candidates);
+    // record data blocks that needs relocation (stripe_id, block_id)
+    vector<pair<int, int> > data_blocks_to_reloc;
+
+    // check the number of data blocks on each node
+    vector<int> node_data_blk_dist(num_nodes, 0);
+    for (int stripe_id = 0; stripe_id < code.lambda_i; stripe_id++) {
+        vector<int> &stripe_indices = stripes[stripe_id]->getStripeIndices();
+        for (int block_id = 0; block_id < code.k_i; block_id++) {
+            int node_id = stripe_indices[block_id];
+            node_data_blk_dist[node_id]++;
+
+            // relocate data blocks start from lambda_f (= 1)
+            if (node_data_blk_dist[node_id] > code.lambda_f) {
+                data_blocks_to_reloc.push_back(pair<int, int>(stripe_id, block_id));
+            }
+        }
+    }
+
+    // create data relocation task for each data block
+    for (auto &item : data_blocks_to_reloc) {
+        int stripe_id = item.first;
+        int block_id = item.second;
+
+        vector<int> &stripe_indices = stripes[stripe_id]->getStripeIndices();
+        int from_node_id = stripe_indices[block_id];
+
+        // Strategy 1: randomly pick a node from data relocation candidates
+        int random_pos = Utils::randomInt(0, reloc_node_candidates.size() - 1, random_generator);
+
+        // relocate node id
+        int reloc_node_id = reloc_node_candidates[random_pos];
+
+        // mark the block as relocated
+        relocated_blk_distribution[reloc_node_id] = true;
+
+        // remove the element from data relocation candidate
+        reloc_node_candidates.erase(reloc_node_candidates.begin() + random_pos);
+
+        // create a data relocation task
+        vector<int> solution;
+        solution.push_back(stripe_id);
+        solution.push_back(block_id);
+        solution.push_back(from_node_id);
+        solution.push_back(reloc_node_id);
+
+        solutions.push_back(solution);
+    }
+
+    // printf("node candidates for block relocation (after PM + DM):\n");
+    // Utils::printIntVector(reloc_node_candidates);
+
+    // printf("reloc_data_dist:\n");
+    // for (auto item : relocated_blk_distribution) {
+    //     printf("%d ", (item == true));
+    // }
+    // printf("\n");
+
+    // for (int node_id = 0; node_id < num_nodes; node_id++) {
+    //     printf("node: %d: ", node_id);
+    //     for (auto item : node_data_blk_dist[node_id]) {
+    //         printf("<%d, %d> ", item.first, item.second);
+    //     }
+    //     printf("\n");
+    // }
     
 }
