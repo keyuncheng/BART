c7502cedb7b495d339239c8d6e0d9113dc7fad05
Keyun Cheng <keyun_cheng@qq.com>
2023-06-01 10:24:59 +0800

    [mod] modify simulation script

--- scripts/compare_sm_bt.py
+++ scripts/compare_sm_bt.py
@@ -2,6 +2,7 @@ import os
 import sys
 import itertools
 import subprocess
+from pathlib import Path
 
 def main():
     ks = [4,6,8,12,16]
@@ -10,21 +11,22 @@ def main():
     # ks = [4]
     # ms = [2]
     # lambdas = [3]
+    num_stripes = [1000]
+    num_nodes_times = [2]
     methods = ["RDRE", "RDPM", "BWRE", "BWPM", "BTRE", "BTPM", "BT"]
     # methods = ["BWRE", "BWPM"]
     # methods = ["BTRE", "BTPM", "BT"]
     num_runs = 5
-    num_stripes = [1000]
-    num_nodes_times = [2]
     placement = "placement"
-    root_dir = "/home/kycheng/Documents/projects/redundancy-transition/load-balance/BalancedConversion"
-    bin_dir = "{}/build".format(root_dir)
+
+    root_dir = Path(os.getcwd()) / ".."
+    bin_dir = root_dir / "build"
 
     # log dir
-    log_dir = "{}/log".format(root_dir)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    cmd = "rm -rf {}/*".format(log_dir)
+    log_dir = root_dir / "log"
+    log_dir.mkdir(exist_ok=True, parents=True)
+
+    # cmd = "rm -rf {}/*".format(log_dir)
     # return_str, stderr = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
     # msg = return_str.decode().strip()
 
@@ -33,25 +35,29 @@ def main():
         if (k < m):
             continue
 
+        # num_nodes
         num_nodes = num_nodes_time * (lambda_ * k + m)
 
         # num_stripe
         if num_stripe % lambda_ > 0:
             num_stripe = (int(num_stripe / lambda_) + 1) * lambda_
 
+        placement_filename = bin_dir / "{}_{}_{}_{}_{}_{}".format(placement, k, m, lambda_, num_nodes, num_stripe)
+
         for i in range(num_runs):
             # generate placement
-            cmd = "cd {}; ./GenPlacement {} {} {} {} {} {} {}".format(bin_dir, k, m, lambda_ * k, m, num_nodes, num_stripe, placement)
+            cmd = "cd {}; ./GenPlacement {} {} {} {} {} {} {}".format(str(bin_dir), k, m, lambda_ * k, m, num_nodes, num_stripe, str(placement_filename))
             print(cmd)
 
             return_str, stderr = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
             msg = return_str.decode().strip()
             print(msg)
 
-            log_filename = "{}/{}_{}_{}_{}_{}.log".format(log_dir, k, m, lambda_, num_nodes, num_stripe)
+            log_filename = log_dir / "{}_{}_{}_{}_{}.log".format(k, m, lambda_, num_nodes, num_stripe)
 
             for method in methods:
-                cmd = "cd {}; ./Simulator {} {} {} {} {} {} {} {} >> {}".format(bin_dir, k, m, lambda_ * k, m, num_nodes, num_stripe, placement, method, log_filename)
+                # run simulation
+                cmd = "cd {}; ./Simulator {} {} {} {} {} {} {} {} >> {}".format(str(bin_dir), k, m, lambda_ * k, m, num_nodes, num_stripe, str(placement_filename), method, str(log_filename))
                 print(cmd)
 
                 return_str, stderr = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()
