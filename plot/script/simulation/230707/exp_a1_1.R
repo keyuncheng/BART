7785d93b48cc3035dba0561d6d63b3e69c238c8d
Theodore Puyang <hpuyang@connect.ust.hk>
2023-07-06 16:07:06 +0000

    [mod] update simulation results 230707

---
+++ plot/script/simulation/230707/exp_a1_1.R
@@ -0,0 +1,35 @@
+# sm_g tasks
+if (T) {
+    x1<-read.table("../../../data/simulation/230707/exp_a1_1.dat", header=TRUE)
+    library(ggplot2)
+    library(grid)
+    library(extrafont)
+    font_import()
+    loadfonts()
+
+    ggplot(x1, aes(x=code_param_id, y=max_load, fill=method_id)) +
+    scale_fill_manual(name=element_blank(),
+                    breaks=c("a","b","c"),
+                    labels=c("RD","BW","BART"),
+                    # values=c(a="#19376D",b="#C38154",c="red")) +
+                    values=c(a='#041562',b='#2187c2',c='#ff0000')) +
+    scale_x_discrete(breaks=c("a","b","c","d","e","f","g","h","i"),
+                    labels=c("(4,2,2)", "(6,2,2)", "(8,2,2)", "(6,3,2)", "(8,3,2)", "(12,3,2)", "(8,4,2)", "(12,4,2)", "(16,4,2)")) +
+    scale_y_continuous(expand = c(0,0),limits=c(0,750), breaks=c(0,100,200,300,400,500,600,700)) +
+    guides(fill=guide_legend(ncol=3, byrow=TRUE, keywidth=0.8, keyheight=0.8)) +
+    #  geom_text(aes(label = max_load), hjust=-2, vjust=-0.4, color = "black", size=2, angle=60, stat="identity") +
+    geom_bar(color="black", stat="identity", width=0.8, position=position_dodge()) +
+    geom_errorbar(aes(ymin=max_load_min, ymax=max_load_max), width=.2, position=position_dodge(.8)) +
+    ylab("Max Load (in Blocks)") +
+    xlab(expression(paste("(k,m,", lambda, ")"))) +
+    theme_classic() +
+    theme(axis.text.x = element_text(size=15, angle=0, hjust=0.5, vjust=1, color="black")) +
+    theme(axis.text.y = element_text(size=15, color="black")) +
+    theme(legend.text = element_text(size=15)) +
+    theme(axis.title.x = element_text(size=15)) +
+    theme(axis.title.y = element_text(size=15)) +
+    theme(legend.text = element_text(size=15)) +
+    theme(legend.background=element_rect(fill = alpha("white", 0.0))) + 
+    theme(legend.position=c(0.5,0.95), legend.direction="vertical", legend.key=element_blank())
+    ggsave("../../../pdf/simulation/230707/exp_a1_1.pdf", width=9, height=4, device=cairo_pdf)
+}
---
+++ plot/script/simulation/230707/exp_a2_2.R
@@ -0,0 +1,40 @@
+if (T) {
+    x1<-read.table("../../../data/simulation/230707/exp_a2_2.dat", header=TRUE)
+    library(ggplot2)
+    library(grid)
+    library(extrafont)
+    font_import()
+    loadfonts()
+
+    ggplot(x1, aes(x=num_stripes_id, y=max_load, group=method_id, shape=method_id, color=method_id)) +
+    scale_color_manual(name=element_blank(),
+                        breaks=c("a","b","c"),
+                        labels=c("RD","BW","BART"),
+                        values=c(a='#041562',b='#2187c2',c='#ff0000')) +
+    scale_shape_manual(name=element_blank(),
+                        breaks=c("a","b","c"),
+                        labels=c("RD","BW","BART"),
+                        values=c(1,4,5)) +
+    scale_linetype_manual(name=element_blank(),
+                        breaks=c("a","b","c"),
+                        labels=c("RD","BW","BART"),
+                        values=c(2,3,4)) +
+    scale_x_discrete(breaks=c("a","b","c","d"),
+                    labels=c("5000", "10000", "15000", "20000")) +
+    scale_y_continuous(expand = c(0,0),limits=c(0,1250), breaks=c(0,200,400,600,800,1000,1200)) +
+    geom_errorbar(aes(ymin=max_load_min, ymax=max_load_max, color=method_id), width=.1) +
+
+    geom_line(linewidth=1, aes(linetype=method_id)) +
+    geom_point(size=3, stroke=1, fill="white") +
+    guides(color=guide_legend(ncol=3, byrow=TRUE, keywidth=0.8, keyheight=0.8)) +
+    ylab("Max Load (in Blocks)") +
+    xlab(expression(paste("M"))) +
+    theme_classic() +
+    theme(axis.text.x = element_text(size=15, angle=0, hjust=0.5, color="black")) +
+    theme(axis.title.y = element_text(size=15)) +
+    theme(axis.text.y = element_text(size=15, color="black")) +
+    theme(legend.text = element_text(size=15)) +
+    theme(axis.title = element_text(size=15)) +
+    theme(legend.position=c(0.5,0.95), legend.direction = "horizontal", legend.key=element_blank())
+    ggsave("../../../pdf/simulation/230707/exp_a2_2.pdf", width=5.5, height=4, device=cairo_pdf)
+}
