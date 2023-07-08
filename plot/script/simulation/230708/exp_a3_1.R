2d0d32a05d64fdd87e068d04d0f2f68f1f5f472d
hpuyang7942 <hpuyang@connect.ust.hk>
2023-07-08 14:13:16 +0000

    [mod] update plots and data

---
+++ plot/script/simulation/230708/exp_a3_1.R
@@ -0,0 +1,42 @@
+if (T) {
+    x1<-read.table("../../../data/simulation/230708/exp_a3_1.dat", header=TRUE)
+    library(ggplot2)
+    library(grid)
+    library(extrafont)
+    font_import()
+    loadfonts()
+
+    ggplot(x1, aes(x=num_stripes_id, y=max_load, group=method_id, shape=method_id, color=method_id)) +
+    scale_color_manual(name=element_blank(),
+                        breaks=c("a","b","c"),
+                        labels=c("RD","BW","BART"),
+                        values=c(a='#1E90FF',b='#C1A61D',c='#EE1111')) +
+    scale_shape_manual(name=element_blank(),
+                        breaks=c("a","b","c"),
+                        labels=c("RD","BW","BART"),
+                        values=c(1,4,5)) +
+    scale_linetype_manual(name=element_blank(),
+                        breaks=c("a","b","c"),
+                        labels=c("RD","BW","BART"),
+                        values=c(2,3,4)) +
+    scale_x_discrete(breaks=c("a","b","c", "d"),
+                    labels=c("5000", "10000", "15000", "20000")) +
+    scale_y_continuous(expand = c(0,0),limits=c(0,900), breaks=c(0,200,400,600,800)) +
+    geom_errorbar(aes(ymin=max_load_min, ymax=max_load_max, color=method_id), width=.1) +
+    # scale_color_manual("method_id", breaks=c(1,2,3),values=c("#0072B2", "#009E73", "#E69F00"))+
+
+    geom_line(linewidth=1, aes(linetype=method_id)) +
+    geom_point(size=5, stroke=1, fill="white") +
+    guides(color=guide_legend(ncol=1, byrow=TRUE, keywidth=0.8, keyheight=0.8), 
+           linetype = guide_legend(override.aes = list(size = 3))) + # Increase the size of the line in the legend
+    ylab("Max Load (in Blocks)") +
+    xlab(expression(italic(paste("M")))) +
+    theme_classic() +
+    theme(axis.text.x = element_text(size=20, angle=0, hjust=0.5, vjust=1, color="black", family="Times New Roman")) +
+    theme(axis.text.y = element_text(size=20, color="black", family="Times New Roman")) +
+    theme(axis.title.x = element_text(size=20, family="Times New Roman")) +
+    theme(axis.title.y = element_text(size=20, family="Times New Roman")) +
+    theme(legend.text = element_text(size=20, family="Times New Roman")) +
+    theme(legend.position=c(0.2,0.8), legend.direction = "horizontal", legend.key=element_blank())
+    ggsave("../../../pdf/simulation/230708/exp_a3_1.pdf", width=5.5, height=4, device=cairo_pdf)
+}
